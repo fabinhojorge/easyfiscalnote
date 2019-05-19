ba3766248af4c55173ab2588e2d4f4f872ac95d6
fabinhojorge <fabinhojorgenet@gmail.com>
2019-05-19 19:06:34 -0300

    Added the second screen that çlist the Fiscal Notes for a given company

--- register/urls.py
+++ register/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from register.views import home
+from register.views import home, company_detail
 
 urlpatterns = [
-    path('', home, name='index')
+    path('', home, name='index'),
+    path('company/<int:id>', company_detail, name='company_detail')
 ]
--- register/views.py
+++ register/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
-from register.models import Company
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from register.models import Company, FiscalNote
 
 
 def home(request):
     data = dict()
     data['companies'] = Company.objects.all()
     return render(request, "register/home.html", data)
+
+
+def company_detail(request, id):
+    data = dict()
+    company = get_object_or_404(Company, id=id)
+    data['company'] = company
+    data['fiscal_notes'] = company.fiscalnote_set.all()
+    return render(request, "register/company_detail.html", data)
