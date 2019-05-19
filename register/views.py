cdbfd234e6094a4d356d83ef42f2b9864f312037
fabinhojorge <fabinhojorgenet@gmail.com>
2019-05-19 18:13:33 -0300

    index page created, listing the companies

--- register/views.py
+++ register/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from register.models import Company
 
 
 def home(request):
     data = dict()
+    data['companies'] = Company.objects.all()
     return render(request, "register/home.html", data)
