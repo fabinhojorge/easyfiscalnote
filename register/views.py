c1599b24d03190f93eb42bb838f6185a21b94771
fabinhojorge <fabinhojorgenet@gmail.com>
2019-05-19 23:08:59 -0300

    Added Fiscal Note details component. Added Fiscal Note search

--- register/views.py
+++ register/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from register.models import Company, FiscalNote
+from django.db.models import Q
 
 
 def home(request):
@@ -12,5 +13,11 @@ def company_detail(request, id):
     data = dict()
     company = get_object_or_404(Company, id=id)
     data['company'] = company
-    data['fiscal_notes'] = company.fiscalnote_set.all()
+
+    search = request.GET.get('search')
+    if request.method == 'GET' and search:
+        data['fiscal_notes'] = company.fiscalnote_set.filter(Q(series__contains=search) | Q(name_description__contains=search))
+    else:
+        data['fiscal_notes'] = company.fiscalnote_set.all()
+
     return render(request, "register/company_detail.html", data)
